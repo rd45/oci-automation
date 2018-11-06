35e26ed62bf44c782fe3f79a73fdb92176d66dc1
RD <richard.dewes@oracle.com>
2018-11-06 11:45:36 +0000

    deleted:    instantiate.sh

--- instantiate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-git clone https://github.com/terraform-providers/terraform-provider-oci.git
-git clone https://github.com/oracle/oci-ansible-modules.git
-
-docker build -t tf-and-ansible ./docker
-docker run --interactive --tty --rm --volume "$PWD":/data tf-and-ansible:latest
