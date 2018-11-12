d6e574c8459f081114d29f1d9b384e469d59a35d
RD <richard.dewes@oracle.com>
2018-11-12 14:42:13 +0000

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook /data/Ansible/main.yaml --extra-vars '{"v_clone_dest":"/data/Terraform/terraform-provider-oci"}'
+ansible-playbook /data/Ansible/main.yaml --extra-vars "@vars.json"
